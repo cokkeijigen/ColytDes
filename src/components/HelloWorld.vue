486309b681a290fa0be7995c0f804a11aecce2a5
GameLef <2824764805zz@gmail.com>
2021-06-23 19:33:15 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup>
+import { defineProps, ref, getCurrentInstance  } from 'vue'
+const { $http: http } = getCurrentInstance().appContext.config.globalProperties
+defineProps({
+  msg: String
+})
+const getBilibili = async () => {
+  const res = await http.get('/bili/v/anime/finish/')
+  console.log('res', res);
+}
+getBilibili()
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
