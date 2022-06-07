78866bc0ea7737d74b445c3a106c82c97ec2b63a
cokkeijigen <67277503+cokkeijigen@users.noreply.github.com>
2022-06-08 02:01:33 +0800

    Update init.js

--- static/main_script/init.js
+++ static/main_script/init.js
@@ -3,3 +3,19 @@
     setMainIframContent("./static/pc-page/main.html");
     loadStyleFile("./static/main_style/main-init.css");
 })()
+
+const pcpageContent = new function() {
+    const thisContent = get("main_ifram_content.id").contentWindow;
+    this.getBannerContent = function() {
+        return thisContent.get("banner_iframe.id").contentWindow;
+    }
+    this.getLeftContent = function() {
+        return thisContent.get("left_iframe.id").contentWindow;
+    }
+    this.getCenterContent = function() {
+        return thisContent.get("center_iframe.id").contentWindow;
+    }
+    this.getRightContent = function() {
+        return thisContent.get("footer_iframe.id").contentWindow;
+    }
+}
